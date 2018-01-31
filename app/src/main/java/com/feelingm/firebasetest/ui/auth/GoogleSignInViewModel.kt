75a5f51304eccefe3f4384869ca8f44b65c081d8
Hyunwoo Jung <feelingm@live.com>
2018-01-31 22:37:52 +0900

    databinding

--- app/src/main/java/com/feelingm/firebasetest/ui/auth/GoogleSignInViewModel.kt
+++ app/src/main/java/com/feelingm/firebasetest/ui/auth/GoogleSignInViewModel.kt
@@ -2,10 +2,10 @@ package com.feelingm.firebasetest.ui.auth
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import com.feelingm.firebasetest.util.databinding.BaseViewModelObservable
 import com.feelingm.firebasetest.util.extensions.runOnIoScheduler
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
-import io.reactivex.disposables.Disposable
 
 /**
  * Created by mac on 2018. 1. 23..
@@ -14,7 +14,7 @@ import io.reactivex.disposables.Disposable
 class GoogleSignInViewModel(val firebaseAuth: FirebaseAuth,
                             val googleSignInClient: GoogleSignInClient,
                             application: Application)
-    : AndroidViewModel(application) {
+    : BaseViewModelObservable(application) {
 
 //    val
 
